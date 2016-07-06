3f93990c39ea51c8b8ce9deaabdd30d3ee4c81fa
rnavagamuwa <randikanavagamuwa@gmail.com>
2016-07-07 01:54:16 +0530

    moved everything into a single loop

--- LoadFile.py
+++ LoadFile.py
@@ -1,10 +1,10 @@
 import csv
+import os
 
 pathToCompleteDataSet = '/home/rnavagamuwa/Documents/CSE/FYP/Datasets/DEBS-2013-SoccerField/full-game.txt'
 pathToBallSeperatedDataSet = '/home/rnavagamuwa/Documents/CSE/FYP/Datasets/DEBS-2013-SoccerField/BallSeperated'
-
-##Firstly, Run below code
-########################################################################################
+if not os.path.isdir(pathToBallSeperatedDataSet):
+   os.makedirs(pathToBallSeperatedDataSet)
 
 completeDataSet = open(pathToCompleteDataSet,'r')
 fout4 = open(pathToBallSeperatedDataSet+'/ball-4.csv', 'w')
@@ -12,21 +12,25 @@ fout8 = open(pathToBallSeperatedDataSet+'/ball-8.csv', 'w')
 fout10 = open(pathToBallSeperatedDataSet+'/ball-10.csv', 'w')
 fout12 = open(pathToBallSeperatedDataSet+'/ball-12.csv', 'w')
 
-line = None
+row =None
+csvWriter = None
 
 def fout4Write():
-    fout4.write(line)
+    csvWriter = csv.writer(fout4)
+    csvWriter.writerow((row[1],row[2],row[3],row[4]))
 
 def fout8Write():
-    fout8.write(line)
+    csvWriter = csv.writer(fout8)
+    csvWriter.writerow((row[1],row[2],row[3],row[4]))
 
 def fout10Write():
-    fout10.write(line)
+    csvWriter = csv.writer(fout10)
+    csvWriter.writerow((row[1],row[2],row[3],row[4]))
 
 def fout12Write():
-    fout12.write(line)
+    csvWriter = csv.writer(fout12)
+    csvWriter.writerow((row[1],row[2],row[3],row[4]))
 
-# map the inputs to the function blocks
 foutOptions = {4: fout4Write,
                8: fout8Write,
                10: fout10Write,
@@ -35,50 +39,9 @@ foutOptions = {4: fout4Write,
 
 for line in completeDataSet:
         try:
-            foutOptions[int(line.split(",")[0])]()
+            row = line.split(",")
+            foutOptions[int(row[0])]()
         except:
             continue
 
 print("Training dataset has been successfully divided into 4 balls")
-
-
-
-#################################################################################################
-# Comment out above code and run following as the second step
-#####################################################################################################
-
-
-
-# import csv
-#
-# with open("E:/FYP/BallSeperated/ball-4.csv", "r") as source:
-#     rdr = csv.reader(source)
-#     with open("E:/FYP/BallSeperated/filer/ball-4.csv", "w", newline='') as result:
-#         wtr = csv.writer(result)
-#         for r in rdr:
-#             wtr.writerow((r[1], r[2], r[3], r[4]))
-#
-# with open("E:/FYP/BallSeperated/ball-8.csv", "r") as source:
-#     rdr = csv.reader(source)
-#     with open("E:/FYP/BallSeperated/filer/ball-8.csv", "w", newline='') as result:
-#         wtr = csv.writer(result)
-#         for r in rdr:
-#             wtr.writerow((r[1], r[2], r[3], r[4]))
-#
-# with open("E:/FYP/BallSeperated/ball-10.csv", "r") as source:
-#     rdr = csv.reader(source)
-#     with open("E:/FYP/BallSeperated/filer/ball-10.csv", "w", newline='') as result:
-#         wtr = csv.writer(result)
-#         for r in rdr:
-#             wtr.writerow((r[1], r[2], r[3], r[4]))
-#
-# with open("E:/FYP/BallSeperated/ball-12.csv", "r") as source:
-#     rdr = csv.reader(source)
-#     with open("E:/FYP/BallSeperated/filer/ball-12.csv", "w", newline='') as result:
-#         wtr = csv.writer(result)
-#         for r in rdr:
-#             wtr.writerow((r[1], r[2], r[3], r[4]))
-#
-#
-#
-# print("Done")
