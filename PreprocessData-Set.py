65943581022bfe121160cbf81e4574c762c01537
rnavagamuwa <randikanavagamuwa@gmail.com>
2016-07-10 00:03:19 +0530

    Optimized

--- PreprocessData-Set.py
+++ PreprocessData-Set.py
@@ -1,3 +1,4 @@
+import datetime
 
 pathToCompleteDataSet = '/home/rnavagamuwa/Documents/CSE/FYP/Datasets/DEBS-2013-SoccerField/full-game.txt'
 pathToPreProcessedDataSet = '/home/rnavagamuwa/Documents/CSE/FYP/Datasets/DEBS-2013-SoccerField/proccesed-full-game.txt'
@@ -8,29 +9,37 @@ preProcessedDataSet = open(pathToPreProcessedDataSet,"w")
 tempRowArr = None
 averageValueDict = dict()
 tempAverageValueRow = None
-# averageValueDict['98'] = [1,2];
-# print(averageValueDict['98'][0])
 
 count =0;
+startTime = datetime.datetime.now()
+
 for line in completeDAtaSet:
-    count = count +1
+    count += 1
     tempRowArr = line.split(",")
     key = tempRowArr[0]
     if key in averageValueDict:
+        # if sensor id is processed already required data will be retreived from the dict
         tempAverageValueRow = averageValueDict[key]
         sidCount = int(tempAverageValueRow[0])
-        sidCount = sidCount + 1
+        sidCount += 1
+        tempAverageValueRow[0] = sidCount
         for index in range(len(tempRowArr)):
-            if index == 0:
+            # Skipping the 1st two columns assuming sensor id and time stamp are always available
+            if index == 0 or index == 1:
                 continue
             if tempRowArr[index]:
+                # Calculating and saving the average
                 tempAverageValueRow[index] = (int(tempAverageValueRow[index]) + int(tempRowArr[index])) / sidCount
             else:
                 tempRowArr[index] = tempAverageValueRow[index]
         preProcessedDataSet.write(",".join(tempRowArr))
     else:
+        # if sensor id is a new one
         preProcessedDataSet.write(",".join(tempRowArr))
         tempRowArr[0] = 0
         averageValueDict[key] = tempRowArr;
-    if count>100:
-        break
+
+endTime = datetime.datetime.now()
+print("Pre processing has been completed.")
+print("Preprocessed row count: "+str(count))
+print("Time taken for preprocessing:" + str(endTime - startTime))
