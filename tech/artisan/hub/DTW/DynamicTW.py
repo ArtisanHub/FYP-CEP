e9fd54869deed29f750e371c1b5d284b2dccbd1d
PravindaPerera <pravinda.joes@gmail.com>
2016-08-02 22:59:32 +0530

    Modified DTW implementation to compare against anomalies

--- tech/artisan/hub/DTW/DynamicTW.py
+++ tech/artisan/hub/DTW/DynamicTW.py
@@ -1,19 +1,22 @@
 import mlpy
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-x = []
-y = []
+a = []
+b = []
+c = []
+d = []
+e = []
 
 indx = 0
-
+dtw_data_limit = 15000
 f = open( 'D:/FYP-Developments/Dataset-Debs-2013/MovingAverageData/resultDTW1.csv', 'rU' ) #open train data
 for line in f:
 
     cells = line.split(",")
-    x.append((float)(cells[7]))
+    a.append((float)(cells[7]))
     indx = indx + 1
 
-    if indx == 10000:
+    if indx == dtw_data_limit:
         break
 
 f.close()
@@ -23,33 +26,74 @@ f = open( 'D:/FYP-Developments/Dataset-Debs-2013/MovingAverageData/resultDTW2.cs
 for line in f:
 
     cells = line.split(",")
-    y.append((float)(cells[7]))
+    b.append((float)(cells[7]))
     indx = indx + 1
 
-    if indx == 10000:
+    if indx == dtw_data_limit:
         break
 
 f.close()
 
-dist, cost, path = mlpy.dtw_std(x, y, dist_only=False)
-mlpy.dtw_subsequence
-print("############")
+indx = 0
+f = open( 'D:/FYP-Developments/Dataset-Debs-2013/MovingAverageData/resultDTW3.csv', 'rU' ) #open train data
+for line in f:
+
+    cells = line.split(",")
+    c.append((float)(cells[7]))
+    indx = indx + 1
+
+    if indx == dtw_data_limit:
+        break
+
+f.close()
+
+indx = 0
+f = open( 'D:/FYP-Developments/Dataset-Debs-2013/MovingAverageData/resultDTW4.csv', 'rU' ) #open train data
+for line in f:
+
+    cells = line.split(",")
+    d.append((float)(cells[7]))
+    indx = indx + 1
+
+    if indx == dtw_data_limit:
+        break
+
+f.close()
+
+indx = 0
+f = open( 'D:/FYP-Developments/Dataset-Debs-2013/MovingAverageData/resultDTW5.csv', 'rU' ) #open train data
+for line in f:
+
+    cells = line.split(",")
+    e.append((float)(cells[7]))
+    indx = indx + 1
+
+    if indx == dtw_data_limit:
+        break
+
+f.close()
+
+dist, cost, path = mlpy.dtw_std(a, d, dist_only=False)
+
+print("Distance between 7th and 28th minutes - Two Golas")
 print(dist)
 print("############")
 
-dist
-0.0
+dist, cost, path = mlpy.dtw_std(b, c, dist_only=False)
 
-fig = plt.figure("DTW")
-ax = fig.add_subplot(111)
-plot1 = plt.imshow(cost.T, origin='lower', cmap=cm.plasma, interpolation='nearest')
-plot2 = plt.plot(path[0], path[1], 'w')
-xlim = ax.set_xlim((-0.5, cost.shape[0]-0.5))
-ylim = ax.set_ylim((-0.5, cost.shape[1]-0.5))
+print("Distance between 13th and 22nd minutes - Two Goals")
+print(dist)
+print("############")
 
+dist, cost, path = mlpy.dtw_std(a, e, dist_only=False)
 
-plt.figure("Path Analysis")
-plt.plot(path[0])
-plt.plot(path[1])
+print("Distance between 7th and 5th minutes")
+print(dist)
+print("############")
 
-plt.show()
+dist, cost, path = mlpy.dtw_std(b, e, dist_only=False)
+
+print("Distance between 13th and 5th minutes")
+print(dist)
+print("############")
+
--- tech/artisan/hub/DTW/LoadData.py
+++ tech/artisan/hub/DTW/LoadData.py
@@ -5,25 +5,34 @@
 
 count1 = 0
 count2 = 0
+count3 = 0
+count4 = 0
+count5 = 0
 
 #obtaining the analysis time frame - forth octet of the match = 7.5 minutes
 
-strt1 = 11473*10**12    #Goal in the 13.22 minute
-end1 = 11593*10**12
+strt1 = 11173*10**12   #Goal in the 7.08 minute
+end1 = 11233*10**12
 
-strt2 = 11113*10**12   #Goal in the 7.08 minute
-end2 = 11233*10**12
+strt2 = 11533*10**12    #Goal in the 13.22 minute
+end2 = 11593*10**12
 
-#strt = 12373*10**12   #Goal in the 28.17 minute
-#end = 12493*10**12
+strt3 = 12073*10**12   #Goal in the 22.12 minute
+end3 = 12133*10**12
 
-strt3 = 11600*10**12   #Non goal period
-end3 = 11680*10**12
+strt4 = 12433*10**12   #Goal in the 28.16 minute
+end4 = 12493*10**12
+
+strt5 = 11053*10**12   #Non goal period - 5th minute
+end5 = 11113*10**12
 
 f = open( 'D:/FYP-Developments/Dataset-Debs-2013/full-game/full-game.csv', 'rU' ) #open train data
 
 r1 = open('D:/FYP-Developments/Dataset-Debs-2013/MovingAverageData/resultDTW1.csv', 'w')
 r2 = open('D:/FYP-Developments/Dataset-Debs-2013/MovingAverageData/resultDTW2.csv', 'w')
+r3 = open('D:/FYP-Developments/Dataset-Debs-2013/MovingAverageData/resultDTW3.csv', 'w')
+r4 = open('D:/FYP-Developments/Dataset-Debs-2013/MovingAverageData/resultDTW4.csv', 'w')
+r5 = open('D:/FYP-Developments/Dataset-Debs-2013/MovingAverageData/resultDTW5.csv', 'w')
 
 #preping the analysis file with the respective window data
 for line in f:
@@ -35,16 +44,38 @@ for line in f:
         r1.write(line)
         count1 = count1 + 1
 
-    elif int(cells[1]) >= strt3 and int(cells[1]) <= end3:
+    elif int(cells[1]) >= strt2 and int(cells[1]) <= end2:
         r2.write(str(count2))
         r2.write(str(","))
         r2.write(line)
         count2 = count2 + 1
+
+    elif int(cells[1]) >= strt3 and int(cells[1]) <= end3:
+        r3.write(str(count3))
+        r3.write(str(","))
+        r3.write(line)
+        count3 = count3 + 1
+
+    elif int(cells[1]) >= strt4 and int(cells[1]) <= end4:
+        r4.write(str(count4))
+        r4.write(str(","))
+        r4.write(line)
+        count4 = count4 + 1
+
+    elif int(cells[1]) >= strt5 and int(cells[1]) <= end5:
+        r5.write(str(count5))
+        r5.write(str(","))
+        r5.write(line)
+        count5 = count5 + 1
+
     else:
         continue
 
 r1.close()
 r2.close()
+r3.close()
+r4.close()
+r5.close()
 f.close()
 
 
