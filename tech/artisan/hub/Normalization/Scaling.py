63d8599ebb71799624722842a0a9fa2dce71b419
PravindaPerera <pravinda.joes@gmail.com>
2016-08-03 23:41:07 +0530

    Normalization of data streams for anomaly detection

--- tech/artisan/hub/Normalization/Scaling.py
+++ tech/artisan/hub/Normalization/Scaling.py
@@ -1,11 +1,10 @@
 from sklearn import preprocessing
-import pandas as pd
 import numpy as np
 from matplotlib import style
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 
-#col0 = []
+col0 = []
 col1 = []
 col2 = []
 col3 = []
@@ -18,15 +17,95 @@ col9 = []
 col10 = []
 col11 = []
 col12 = []
-col13 = []
 
-salihPath = "/home/jawadhsr/Desktop/test.csv"
-f = open( salihPath, 'rU' )
+temp = []
+
+left_margin = -0.5
+right_margin = 0.5
+
+sep = ","
+
+def extractData():
+
+    count = 0
+    for itm in col6:
+        if itm < left_margin or itm > right_margin:
+            e_data_row = str(col0[count]) + sep + str(col1[count]) + sep + str(col2[count]) + sep + str(
+                col3[count]) + sep + str(
+                col4[count]) + sep + str(col5[count]) + sep + str(col6[count]) \
+                         + sep + str(col7[count]) + sep + str(col8[count]) + sep + str(col9[count]) + sep + str(
+                col10[count]) + sep + str(col11[count]) + sep + str(col12[count]) \
+                         + str("\n")
+
+            output.write(e_data_row)
+            temp.append(count)
+        count = count + 1
+
+    count = 0
+    for itm in col5:
+        if itm < left_margin or itm > right_margin:
+            e_data_row = str(col0[count]) + sep + str(col1[count]) + sep + str(col2[count]) + sep + str(
+                col3[count]) + sep + str(
+                col4[count]) + sep + str(col5[count]) + sep + str(col6[count]) \
+                         + sep + str(col7[count]) + sep + str(col8[count]) + sep + str(col9[count]) + sep + str(
+                col10[count]) + sep + str(col11[count]) + sep + str(col12[count]) \
+                         + str("\n")
+
+            output.write(e_data_row)
+            temp.append(count)
+        count = count + 1
+
+    count = 0
+    for itm in col4:
+        if itm < left_margin or itm > right_margin:
+            e_data_row = str(col0[count]) + sep + str(col1[count]) + sep + str(col2[count]) + sep + str(
+                col3[count]) + sep + str(
+                col4[count]) + sep + str(col5[count]) + sep + str(col6[count]) \
+                         + sep + str(col7[count]) + sep + str(col8[count]) + sep + str(col9[count]) + sep + str(
+                col10[count]) + sep + str(col11[count]) + sep + str(col12[count]) \
+                         + str("\n")
+
+            output.write(e_data_row)
+            temp.append(count)
+        count = count + 1
+
+    count = 0
+    for itm in col3:
+        if itm < left_margin or itm > right_margin:
+            e_data_row = str(col0[count]) + sep + str(col1[count]) + sep + str(col2[count]) + sep + str(
+                col3[count]) + sep + str(
+                col4[count]) + sep + str(col5[count]) + sep + str(col6[count]) \
+                         + sep + str(col7[count]) + sep + str(col8[count]) + sep + str(col9[count]) + sep + str(
+                col10[count]) + sep + str(col11[count]) + sep + str(col12[count]) \
+                         + str("\n")
+
+            output.write(e_data_row)
+            temp.append(count)
+        count = count + 1
+
+    count = 0
+    for itm in col2:
+        if itm < left_margin or itm > right_margin:
+            e_data_row = str(col0[count]) + sep + str(col1[count]) + sep + str(col2[count]) + sep + str(
+                col3[count]) + sep + str(
+                col4[count]) + sep + str(col5[count]) + sep + str(col6[count]) \
+                         + sep + str(col7[count]) + sep + str(col8[count]) + sep + str(col9[count]) + sep + str(
+                col10[count]) + sep + str(col11[count]) + sep + str(col12[count]) \
+                         + str("\n")
+
+            output.write(e_data_row)
+            temp.append(count)
+        count = count + 1
+
+    return
+
+
+f = open( "D:/FYP-Developments/Dataset-Debs-2013/full-game/full-game.csv", 'rU' )
 
 for line in f:
 
     cells = line.split(",")
-    #col0.append((float)(cells[0]))
+    col0.append((float)(cells[0]))
     col1.append((float)(cells[1]))
     col2.append((float)(cells[2]))
     col3.append((float)(cells[3]))
@@ -39,59 +118,37 @@ for line in f:
     col10.append((float)(cells[10]))
     col11.append((float)(cells[11]))
     col12.append((float)(cells[12]))
-    col13.append((float)(cells[13]))
 
 f.close()
 
-col3_scaled = preprocessing.scale(col3)
-col4_scaled = preprocessing.scale(col4)
-col5_scaled = preprocessing.scale(col5)
+col2 = preprocessing.scale(col2)
+col3 = preprocessing.scale(col3)
+col4 = preprocessing.scale(col4)
+col5 = preprocessing.scale(col5)
+col6 = preprocessing.scale(col6)
+
+output = open('D:/FYP-Developments/Dataset-Debs-2013/MovingAverageData/resultNormalised.csv', 'w')
+
+extractData()
+
+output.close()
+temp.clear()
+
+print("Data Extracting complete")
+print("-------------------------------")
 
 
-print("-------------------")
-print(col3)
-print("##")
-print(col3_scaled)
-print("###")
-print(col3_scaled.mean(axis=0))
-print("####")
-print(col3_scaled.std(axis=0))
-print("-------------------")
-
-print("-------------------")
-print(col4)
-print("##")
-print(col4_scaled)
-print("###")
-print(col4_scaled.mean(axis=0))
-print("####")
-print(col4_scaled.std(axis=0))
-print("-------------------")
-
-print("-------------------")
-print(col5)
-print("##")
-print(col5_scaled)
-print("###")
-print(col5_scaled.mean(axis=0))
-print("####")
-print(col5_scaled.std(axis=0))
-print("-------------------")
-
-style.use('fivethirtyeight')
-
-plt.figure("Column 3 - Scaled")
-plt.plot(col3_scaled,mlab.normpdf(col3_scaled,np.mean(col3_scaled),np.std(col3_scaled)), label="Data")
-plt.legend(loc='upper right')
-
-plt.figure("Column 4 - Scaled")
-plt.plot(col4_scaled, label="Data")
-plt.legend(loc='upper right')
-
-plt.figure("Column 5 - Scaled")
-plt.plot(col5_scaled, label="Data")
-plt.legend(loc='upper right')
-
-
-plt.show()
+# style.use('fivethirtyeight')
+#
+# plt.figure("Column 3 - Scaled")
+# plt.plot(col3,mlab.normpdf(col3,np.mean(col3),np.std(col3)))
+#
+# plt.figure("Column 4 - Scaled")
+# plt.plot(col4,mlab.normpdf(col4,np.mean(col4),np.std(col4)))
+#
+# plt.figure("Column 5 - Scaled")
+# plt.plot(col5,mlab.normpdf(col5,np.mean(col5),np.std(col5)))
+#
+#
+# plt.show()
 
